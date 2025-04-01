9a1cb2a4adc7416848dfc0b5d951a8476dc85543
m00nwtchr <m00nwtchr@duck.com>
2025-04-01 22:15:09 +0200

    Use crane in nix flake

--- flake.nix
+++ flake.nix
@@ -1,87 +1,136 @@
 {
-  description = "A devShell example";
+  description = "Chronicles of Darkness Player Companion Nix Flake";
+
   inputs = {
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
+
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
+    crane,
     flake-utils,
+    rust-overlay,
+    android,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
         overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+      };
+      inherit (pkgs) lib;
+
+      # rustToolchainFor = p: (p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+      rustToolchainFor = p:
+        p.rust-bin.selectLatestNightlyWith (
+          t: t.default
+        );
+      rustDevToolchainFor = p:
+        (rustToolchainFor p).override {
+          extensions = ["rust-src" "rust-analyzer"];
         };
-        rustBin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        deps = with pkgs; [
-          libxkbcommon
-          libGL
-          wayland
-          xdotool
-          glib
-          gtk3
-          webkitgtk_4_1
-          pango
-          cairo
-          gdk-pixbuf
-          libsoup_3
-          wasm-bindgen-cli_0_2_100
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchainFor;
+      craneDev = craneLib.overrideToolchain rustDevToolchainFor;
+
+      src = lib.cleanSourceWith {
+        src = self; # The original, unfiltered source
+        filter = path: type:
+          !(lib.hasSuffix "tailwind.css" path) # Generated CSS
+          && ((lib.hasSuffix "input.css" path) # Tailwind CSS Input
+            || (lib.hasInfix "/assets/" path) # Assets
+            || (craneLib.filterCargoSources path type)); # .rs
+      };
+
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        buildInputs = with pkgs;
+          (lib.optionals stdenv.isLinux
+            [
+              webkitgtk_4_1
+              xdotool
+              wayland
+            ])
+          ++ lib.optionals stdenv.isDarwin (
+            with darwin.apple_sdk.frameworks; [
+              IOKit
+              Carbon
+              WebKit
+              Security
+              Cocoa
+            ]
+          );
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
         ];
-      in {
-        devShells.default = with pkgs;
-          mkShell rec {
-            nativeBuildInputs = [pkg-config];
-            buildInputs =
-              [
-                (rustBin.override {
-                  extensions = ["rust-docs" "rust-src" "clippy" "rust-analyzer"];
-                })
-                (rust-bin.selectLatestNightlyWith (toolchain:
-                    toolchain.rustfmt))
+      };
 
-                dioxus-cli
-                watchman
-                tailwindcss_4
-              ]
-              ++ deps;
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-            #CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.llvmPackages.clangUseLLVM}/bin/clang";
-            #RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
-            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
-          };
-        packages.default = with pkgs; let
-          rustPlatform = makeRustPlatform {
-            cargo = rustBin;
-            rustc = rustBin;
-          };
-        in
-          rustPlatform.buildRustPackage rec {
-            pname = "cofd-pc";
-            version = "1.0.0";
-            src = ./.;
-            cargoBuildFlags = "-p cofd-pc";
+      cofd-pc = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
 
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              allowBuiltinFetchGit = true;
-            };
+          nativeBuildInputs = with pkgs;
+            commonArgs.nativeBuildInputs
+            ++ [
+              tailwindcss_4
+              dioxus-cli
+            ];
 
-            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          preBuild = ''
+            tailwindcss -i input.css -o assets/tailwind.css
+          '';
 
-            nativeBuildInputs = [pkg-config];
-            buildInputs =
-              [
-              ]
-              ++ deps;
-          };
-      }
-    );
+          cargoBuildCommand = "dx build --release --platform desktop --";
+          installPhaseCommand = ''
+            install -Dm755 target/dx/cofd-pc/release/linux/app/cofd-pc $out/bin/cofd-pc
+          '';
+
+          postInstall = ''
+            cp -a target/dx/cofd-pc/release/linux/app/assets $out/bin/
+          '';
+        });
+    in {
+      checks = {
+        inherit cofd-pc;
+
+        # Run clippy
+        cofd-pc-clippy = craneLib.cargoClippy (commonArgs
+          // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+        # Check formatting
+        cofd-pc-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        cofd-pc-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+      };
+
+      packages.default = cofd-pc;
+      apps.default = flake-utils.lib.mkApp {
+        drv = cofd-pc;
+      };
+
+      devShells.default = craneDev.devShell {
+        checks = self.checks.${system};
+
+        packages = [
+          pkgs.wasm-bindgen-cli_0_2_100
+        ];
+      };
+    });
 }
